47833ba5f0ec9d8f0b6cc512a66eb5033fd0dfcd
DayDay4two0 <152438852+grudininegor407@users.noreply.github.com>
2024-02-04 21:01:44 +0530

    Create displayGame.swift

---
+++ displayGame.swift
@@ -0,0 +1,5 @@
+  public function displayGame() {
+        echo '<html lang="en">
+                <head>
+                    <meta charset="UTF-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
